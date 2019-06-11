cfaed4e0aea925a6d7d8471f2ab7a790eebaca30
Mary Bucknell <mbucknell@usgs.gov>
2019-06-11 15:26:59 -0500

    Added unit tests. Updated README

---
+++ src/test/java/gov/acwi/wqp/etl/natdb/BaseProcessorTest.java
@@ -0,0 +1,31 @@
+package gov.acwi.wqp.etl.natdb;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.mockito.Mockito;
+
+public class BaseProcessorTest {
+	
+	private BaseProcessor absCls = Mockito.mock(BaseProcessor.class, Mockito.CALLS_REAL_METHODS);
+	
+	@Test
+	public void noLeadingOrTrailingSpaceTest() {
+		assertEquals(absCls.trimString("My Name"), "My Name");
+	}
+	
+	@Test
+	public void leadingWhitespaceTest() {
+		assertEquals(absCls.trimString("\t My Name"), "My Name");
+	}
+	
+	@Test
+	public void trailingWhitespaceTest() {
+		assertEquals(absCls.trimString("My Name \t"), "My Name");
+	}
+	
+	@Test
+	public void leadingAndTrailingWhitespacTest() {
+		assertEquals(absCls.trimString("  \t My Name  \n"), "My Name");
+	}
+
+}
